025302bee7193a5c2aa9eba1e398cf1bc89618dd
Josiane Silva <josianesouza8752@gmail.com>
2023-03-20 16:45:35 -0300

    terminated componentization of  the pages products details and catalog

---
+++ src/components/CategoriesContainer/index.tsx
@@ -0,0 +1,16 @@
+import './styles.css'
+
+export default function CategoriesContainer() {
+
+    return (
+
+        <div className="dsc-category-container">
+            <div className="dsc-category">
+                Eletrônicos
+            </div>
+            <div className="dsc-category">
+                Computadores
+            </div>
+        </div>
+    );
+}
