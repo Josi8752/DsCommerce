46bc56207f0d4bd9a7eb091aa1b1da6a04174db9
Josiane Silva <josianesouza8752@gmail.com>
2023-05-15 12:52:59 -0300

    cart in local storage

---
+++ src/services/cart-service.ts
@@ -0,0 +1,13 @@
+import { OrderDTO } from "../models/order";
+import * as cartRepository from '../localStorage/cart-repository';
+
+export function saveCart(cart: OrderDTO){
+    cartRepository.save(cart);
+}
+
+export function getCart(){
+    return (
+        OrderDTO
+    )
+}
+
