e41590cfa9a4d6cbacf7cea133650d502ea6845d
Josiane Silva <josianesouza8752@gmail.com>
2023-05-18 15:52:18 -0300

    Query params

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -6,24 +6,31 @@ import * as productService from '../../../services/product-service';
 import './styles.css';
 import { ProductDTO } from '../../../models/product';
 
+type QueryParams ={
+  page: number,
+  name: string
+}
 
 export default function Catalog() {
 
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
-const [productName, setProductName] = useState('');
+const [queryParams, setQueryParams] = useState<QueryParams>({
+  page: 0,
+  name: ""
+});
 
   useEffect(() => {
 
-    productService.findPageRequest(0, productName)
+    productService.findPageRequest(queryParams.page, queryParams.name)
       .then(response => {
         setProducts(response.data.content);
       })
-  }, [productName]);
+  }, [queryParams]);
 
 
   function handleSearch(searchText: string){
-    setProductName(searchText)
+    setQueryParams( {...queryParams, name:searchText});
   }
   return (
 
