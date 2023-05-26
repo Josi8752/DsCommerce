c053fa76a93850181bfc8eb3baa33912ec078e6f
Josiane Silva <josianesouza8752@gmail.com>
2023-05-26 13:21:10 -0300

    Interceptors

--- frontEnd/src/utils/request.ts
+++ frontEnd/src/utils/request.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { BASE_URL } from "./system";
 import * as authService from '../services/auth-service';
+import { Navigate } from "react-router-dom";
 
 export function requestBackend(config: AxiosRequestConfig) {
 
@@ -14,3 +15,37 @@ export function requestBackend(config: AxiosRequestConfig) {
     return axios({ ...config, baseURL: BASE_URL, headers });
 
 }
+
+
+// REQUEST INTERCEPTOR
+
+axios.interceptors.request.use(
+    function (config) {
+
+        // DO SOMETHING BEFORE REQUEST IS SENT
+        return config;
+    },
+    function (error) {
+
+        // DO SOMETHING WITH REQUEST ERROR
+        return Promise.reject(error);
+    }
+);
+
+
+// RESPONSE INTERCEPTOR
+
+axios.interceptors.response.use(
+    function (response) {
+
+        // DO SOMETHING WITH RESPONSE DATA IF STATUS IS 2xx
+
+        return response;
+    },
+
+    function (error) {
+        // DO SOMETHING WITH RESPONSE ERROR
+
+        return Promise.reject(error);
+    }
+);
