3c8cd41a4f774578f563be07730900348a1013cc
Josiane Silva <josianesouza8752@gmail.com>
2023-05-28 17:59:57 -0300

    Product CRUD static layout

--- frontEnd/src/routes/Admin/ProductListing /index.tsx
+++ frontEnd/src/routes/Admin/ProductListing /index.tsx
@@ -1,6 +1,71 @@
-export default function ProductListing() {
+import './styles.css';
+import { ProductDTO } from '../../../models/product';
+import imgIconEdit from '../../../assets/edit.svg';
+import imgIconDelete from '../../../assets/delete.svg';
+import imgComputer from '../../../assets/computer.png';
+
+type Props = {
+    product: ProductDTO;
+}
+export default function ProductListing({ product }: Props) {
 
     return (
-        <h1>Listagem</h1>
+        <main>
+            <section id="product-listing-section" className="dsc-container">
+                <h2 className="dsc-section-title dsc-mb20">Cadastro de produtos</h2>
+
+                <div className="dsc-btn-page-container dsc-mb20">
+                    <div className="dsc-btn dsc-btn-white">Novo</div>
+                </div>
+
+                <form className="dsc-search-bar">
+                    <button type="submit">🔎︎</button>
+                    <input type="text" placeholder="Nome do produto" />
+                    <button type="reset">X</button>
+                </form>
+
+                <table className="dsc-table dsc-mb20 dsc-mt20">
+                    <thead>
+                        <tr>
+                            <th className="dsc-tb576">ID</th>
+                            <th></th>
+                            <th className="dsc-tb768">Preço</th>
+                            <th className="dsc-txt-left">Nome</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="dsc-tb576">341</td>
+                            <td><img className="dsc-product-listing-image" src={imgComputer} alt="Computer" /></td>
+                            <td className="dsc-tb768">R$ 5000,00</td>
+                            <td className="dsc-txt-left">Computador Gamer XT Plus Ultra</td>
+                            <td><img className="dsc-product-listing-btn" src={imgIconEdit} alt="Editar" /></td>
+                            <td><img className="dsc-product-listing-btn" src={imgIconDelete} alt="Deletar" /></td>
+                        </tr>
+                        <tr>
+                            <td className="dsc-tb576">341</td>
+                            <td><img className="dsc-product-listing-image" src={imgComputer} alt="Computer" /></td>
+                            <td className="dsc-tb768">R$ 5000,00</td>
+                            <td className="dsc-txt-left">Computador Gamer XT Plus Ultra</td>
+                            <td><img className="dsc-product-listing-btn" src={imgIconEdit} alt="Editar" /></td>
+                            <td><img className="dsc-product-listing-btn" src={imgIconDelete} alt="Deletar" /></td>
+                        </tr>
+                        <tr>
+                            <td className="dsc-tb576">341</td>
+                            <td><img className="dsc-product-listing-image" src={imgComputer} alt="Computer" /></td>
+                            <td className="dsc-tb768">R$ 5000,00</td>
+                            <td className="dsc-txt-left">Computador Gamer XT Plus Ultra</td>
+                            <td><img className="dsc-product-listing-btn" src={imgIconEdit} alt="Editar" /></td>
+                            <td><img className="dsc-product-listing-btn" src={imgIconDelete} alt="Deletar" /></td>
+                        </tr>
+
+                    </tbody>
+                </table>
+
+                <div className="dsc-btn-next-page">Carregar mais</div>
+            </section>
+        </main>
     );
 }
