d3ee1b2981356347d312ddbecd4fc955583a0d0c
Josiane Silva <josianesouza8752@gmail.com>
2023-05-28 17:08:24 -0300

    Route of admin

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,22 +1,25 @@
 
 import { Navigate, Route, Routes } from 'react-router-dom';
-import ClientHome from './routes/ClientHome';
-import ProductsDetails from './routes/ClientHome/ProductsDetails';
-import Catalog from './routes/ClientHome/Catalog';
-import Cart from './routes/ClientHome/Cart';
 import { useEffect, useState } from 'react';
 import { ContextCartCount } from './utils/context-cart';
-import Login from './routes/ClientHome/Login';
-import Admin from './routes/Admin';
-import AdminHome from './routes/Admin/AdminHome';
 import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
 import { history } from './utils/history';
 import { PrivateRoute } from './components/PrivateRoute';
 import { AccessTokenPayloadDTO } from './models/auth';
 import { ContextToken } from './utils/context-token';
+import ClientHome from './routes/ClientHome';
+import ProductsDetails from './routes/ClientHome/ProductsDetails';
+import Catalog from './routes/ClientHome/Catalog';
+import Cart from './routes/ClientHome/Cart';
+import Login from './routes/ClientHome/Login';
+import Admin from './routes/Admin';
+import AdminHome from './routes/Admin/AdminHome';
+import Confirmation from './routes/ClientHome/Confirmation';
+import ProductListing from './routes/Admin/ProductListing ';
+import ProductForm from './routes/Admin/ProductForm';
 import * as authService from './services/auth-service';
 import * as cartService from './services/cart-service';
-import Confirmation from './routes/ClientHome/Confirmation';
+
 
 export default function App() {
 
@@ -48,7 +51,10 @@ export default function App() {
               <Route path="confirmation/:orderId" element={<PrivateRoute><Confirmation /></PrivateRoute>} />
             </Route>
             <Route path="/admin/" element={<PrivateRoute roles={['ROLE_ADMIN']}><Admin /></PrivateRoute>}>
-              <Route index element={<AdminHome />} />
+              <Route index element={<Navigate to='/admin/home' />} />
+              <Route path='home' element={<AdminHome />} />
+              <Route path='products' element={<ProductListing />} />
+              <Route path="products/:productId" element={<ProductForm />} />
             </Route>
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
