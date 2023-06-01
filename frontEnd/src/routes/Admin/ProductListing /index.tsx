522e2f38e576cf508fce2cd8d297022cf55867b2
Josiane Silva <josianesouza8752@gmail.com>
2023-06-01 14:12:48 -0300

    Dirty and validate all

--- frontEnd/src/routes/Admin/ProductListing /index.tsx
+++ frontEnd/src/routes/Admin/ProductListing /index.tsx
@@ -67,6 +67,9 @@ export default function ProductListing() {
     function handleDeleteClick(productId: number) {
         setDialogConfirmationData({ ...dialogConfirmationData, id: productId, visible: true });
     }
+    function handleUpdateClick(productId: number) {
+        navigate(`/admin/products/${productId}`);
+    }
 
     function handleDialogConfirmationAnswer(answer: boolean, productId: number) {
 
@@ -129,7 +132,7 @@ export default function ProductListing() {
                                     <td><img className="dsc-product-listing-image" src={product.imgUrl} alt={product.name} /></td>
                                     <td className="dsc-tb768">R$ {product.price.toFixed(2)}</td>
                                     <td className="dsc-txt-left">{product.name}</td>
-                                    <td><img className="dsc-product-listing-btn" src={imgIconEdit} alt="Editar" /></td>
+                                    <td><img onClick={() => handleUpdateClick(product.id)} className="dsc-product-listing-btn" src={imgIconEdit} alt="Editar" /></td>
                                     <td><img onClick={() => handleDeleteClick(product.id)} className="dsc-product-listing-btn" src={imgIconDelete} alt="Deletar" /></td>
                                 </tr>
                             ))
